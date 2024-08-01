0bf4a2a9ca9ed94a011b4b33b396a30fd1f77d21
DanOcho8 <98377769+DanOcho8@users.noreply.github.com>
2024-07-31 20:24:02 -0700

    fixed sidebar

---
+++ CodeAgainstCancer/static/scripts.js
@@ -0,0 +1,17 @@
+var mini = true;
+
+function toggleSidebar() {
+  if (mini) {
+    console.log("opening sidebar");
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    this.mini = false;
+  } else {
+    console.log("closing sidebar");
+    document.getElementById("mySidebar").style.width = "50px";
+    document.getElementById("main").style.marginLeft = "50px";
+    this.mini = true;
+  }
+}
+
+
